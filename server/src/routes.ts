ffafc0a4992107c998214525c1e3a1b4f80f6584
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2023-01-19 22:42:17 -0300

    feat: isolating files

---
+++ server/src/routes.ts
@@ -0,0 +1,10 @@
+import { prisma } from './lib/prisma';
+import { FastifyInstance } from 'fastify';
+
+export async function appRoutes(app: FastifyInstance) {
+  app.get('/hello', async () => {
+    const habits = await prisma.habit.findMany();
+
+    return habits;
+  });
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,12 @@
 import fastify from 'fastify';
 import cors from '@fastify/cors';
-import { PrismaClient } from '@prisma/client';
+
+import { appRoutes } from './routes';
 
 const app = fastify();
-const prisma = new PrismaClient();
 
 app.register(cors);
-
-app.get('/hello', async () => {
-  const habits = await prisma.habit.findMany();
-
-  return habits;
-});
+app.register(appRoutes);
 
 app
   .listen({
