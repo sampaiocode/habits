d2b3b64c2e585d6ee396fa564adc7d739423d2d7
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2023-01-28 10:49:24 -0300

    feat: create basic structure of screens

---
+++ mobile/src/screens/Habit.tsx
@@ -0,0 +1,5 @@
+import { View } from 'react-native';
+
+export function Habit() {
+  return <View className="bg-background pt-16 flex-1"></View>;
+}
---
+++ mobile/src/screens/New.tsx
@@ -0,0 +1,5 @@
+import { View } from 'react-native';
+
+export function New() {
+  return <View className="bg-background pt-16 flex-1"></View>;
+}
