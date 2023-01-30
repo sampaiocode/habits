0f0e60f46d7cff8d9b12b0a092b047192e2152bf
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2023-01-29 21:11:35 -0300

    feat: add navigation

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,10 +1,13 @@
 import { View, TouchableOpacity, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { Feather } from '@expo/vector-icons';
-import colors from 'tailwindcss/colors';
 
+import colors from 'tailwindcss/colors';
 import LogoImg from '../assets/logo.svg';
 
 export function Header() {
+  const { navigate } = useNavigation();
+
   return (
     <View className="w-full flex-row items-center justify-between">
       <LogoImg />
@@ -12,6 +15,7 @@ export function Header() {
       <TouchableOpacity
         activeOpacity={0.7}
         className="h-11 px-4 border border-violet-500 rounded-lg flex-row items-center"
+        onPress={() => navigate('new')}
       >
         <Feather name="plus" color={colors.violet[500]} size={20} />
         <Text className="text-white text-base font-semibold ml-3 ">Novo</Text>
