2667febfde277c318520a246e6e49d1b6fc884f2
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2023-02-01 22:47:12 -0300

    feat: add habits record interface

---
+++ mobile/src/components/Checkbox.tsx
@@ -0,0 +1,25 @@
+import { TouchableOpacity, TouchableOpacityProps, View, Text } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
+import colors from 'tailwindcss/colors';
+
+interface CheckboxProps extends TouchableOpacityProps {
+  title: string;
+  checked?: boolean;
+}
+
+export function Checkbox({ title, checked = false, ...rest }: CheckboxProps) {
+  return (
+    <TouchableOpacity activeOpacity={0.7} className="flex-row mb-2 items-center" {...rest}>
+      {checked ? (
+        <View className="bg-green-500 h-8 w-8 rounded-lg items-center justify-center">
+          <Feather name="check" color={colors.white} />
+        </View>
+      ) : (
+        <View className="bg-zinc-900 h-8 w-8 rounded-lg" />
+      )}
+
+      <Text className="text-white ml-3 text-base">{title}</Text>
+    </TouchableOpacity>
+  );
+}
--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,5 +1,71 @@
-import { View } from 'react-native';
+import { useState } from 'react';
+import { ScrollView, View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
+import { BackButton } from '../components/BackButton';
+import { Checkbox } from '../components/Checkbox';
+
+import colors from 'tailwindcss/colors';
+
+const availableWeekDays = [
+  'Domingo',
+  'Segunda-feira',
+  'Terça-feira',
+  'Quarta-feira',
+  'Quinta-feira',
+  'Sexta-feira',
+  'Sábado'
+];
 
 export function New() {
-  return <View className="bg-background pt-16 flex-1"></View>;
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+
+  function handleToggleWeekDay(weekDayIndex: number) {
+    if (weekDays.includes(weekDayIndex)) {
+      setWeekDays(prevState => prevState.filter(weekDay => weekDay != weekDayIndex));
+    } else {
+      setWeekDays(prevState => [...prevState, weekDayIndex]);
+    }
+  }
+
+  return (
+    <View className="bg-background pt-16 flex-1">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <BackButton />
+
+        <Text className="text-white text-3xl mt-6 font-extrabold">Criar hábito</Text>
+
+        <Text className="text-white text-base mt-6 font-semibold">Qual seu comprometimento?</Text>
+
+        <TextInput
+          className="bg-zinc-800 text-white h-12 pl-4 mt-3 rounded-lg focus:border-2 focus:border-green-600"
+          placeholder="Exercícios, dormir bem, etc..."
+          placeholderTextColor={colors.zinc[400]}
+        />
+
+        <Text className="text-white text-base font-semibold mt-4 mb-3">Qual a recorrência?</Text>
+
+        {availableWeekDays.map((weekDay, i) => (
+          <Checkbox
+            key={weekDay}
+            title={weekDay}
+            checked={weekDays.includes(i)}
+            onPress={() => handleToggleWeekDay(i)}
+          />
+        ))}
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="bg-green-600 w-full h-14 rounded-md mt-6 flex-row items-center justify-center"
+        >
+          <Feather name="check" size={20} color={colors.white} />
+
+          <Text className="text-white font-semibold text-base ml-2">Confirmar</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </View>
+  );
 }
