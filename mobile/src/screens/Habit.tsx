784e71e8e8c86bf8c4bb0d3ec9f9520053238fad
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2023-02-19 18:37:26 -0300

    feat: create HabitsEmpty component

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -10,6 +10,7 @@ import { BackButton } from '../components/BackButton';
 import { ProgressBar } from '../components/ProgressBar';
 import { Checkbox } from '../components/Checkbox';
 import { Loading } from '../components/Loading';
+import { HabitsEmpty } from '../components/HabitsEmpty';
 
 interface Params {
   date: string;
@@ -90,7 +91,7 @@ export function Habit() {
         <ProgressBar progress={habitsProgress} />
 
         <View className="mt-6">
-          {dayInfo?.possibleHabits &&
+          {dayInfo?.possibleHabits ? (
             dayInfo?.possibleHabits.map(habit => (
               <Checkbox
                 key={habit.id}
@@ -98,7 +99,10 @@ export function Habit() {
                 checked={completedHabits.includes(habit.id)}
                 onPress={() => handleToggleHabit(habit.id)}
               />
-            ))}
+            ))
+          ) : (
+            <HabitsEmpty />
+          )}
         </View>
       </ScrollView>
     </View>
