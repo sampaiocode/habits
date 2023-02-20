5e95b1cf60a1fb354919a99ba04c55c4fb3a595b
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2023-02-20 12:21:42 -0300

    feat: using useFocusEffect to refresh the homepage when returning

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useState, useCallback } from 'react';
 import { View, Text, ScrollView, Alert } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 
 import dayjs from 'dayjs';
 
@@ -42,9 +42,11 @@ export function Home() {
     }
   }
 
-  useEffect(() => {
-    fechtData();
-  }, []);
+  useFocusEffect(
+    useCallback(() => {
+      fechtData();
+    }, [])
+  );
 
   if (loading) {
     return <Loading />;
